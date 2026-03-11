fe1ef46fe01778eb352c590663afd5d03ab880ae
Hajime Morita <haji2404@gmail.com>
2026-03-11 15:09:52 +0900

    fix: web package version for Vercel build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/flutter/flutter.git -b stable --depth 1 $HOME/flutter
+git clone https://github.com/flutter/flutter.git -b 3.24.0 --depth 1 $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 flutter precache --web
 flutter build web --release
